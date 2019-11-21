ccb38f58fc91a3daaaced3fa5670dfd788ef1d6e
Martin Van Aken <martin@joyouscoding.com>
2019-11-21 07:29:53 +0200

    Don't get user in production

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,16 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { dhis2 } from '../dhis2-config';
+import { isDevMode } from '@angular/core';
 
 const user = dhis2.user;
 const password = dhis2.password;
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Authorization': "Basic " + btoa(user + ":" + password)
-  })
-};
+var httpOptions;
+
+if (isDevMode()) {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': "Basic " + btoa(user + ":" + password)
+    })
+  };
+} else {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+}
+ 
 
 @Injectable({
   providedIn: 'root'
